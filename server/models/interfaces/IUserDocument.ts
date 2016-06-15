c025e8e2aebf14dba1547919659332f1e5bda3d1
iammen7 <iammen7@gmail.com>
2016-06-15 14:22:13 +0700

    Delete unused files

--- server/models/interfaces/IUserDocument.ts
+++
@@ -1,6 +0,0 @@
-import {Document} from "mongoose";
-
-export interface IUserDocument extends Document {
-    name: string;
-    password: string;
-}
--- server/repositories/UserRepository.ts
+++
@@ -1,9 +0,0 @@
-import {IUserDocument} from "../models/interfaces/IUserDocument";
-import {BaseRepository} from "./BaseRepository";
-import UserModel = require("../models/UserModel");
-
-export class UserRepository extends BaseRepository<IUserDocument> {
-    constructor () {
-        super(UserModel);
-    }
-}
